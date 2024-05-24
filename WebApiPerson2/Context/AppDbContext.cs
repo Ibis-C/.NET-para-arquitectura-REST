5f5085bab3a3b7850b41bc6b324af56af3d62ae9
usuario <usuario@DESKTOP-K7V21MQ>
2024-05-24 13:02:04 -0600

    Agregar archivos de proyecto.

---
+++ WebApiPerson2/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson2.Models;
+
+namespace WebApiPerson2.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base (options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
